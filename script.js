e835300f5d47983b2e1558db1189603543b33a92
Dat P Nguyen <darkexpert3@gmail.com>
2024-07-26 04:38:28 +0700

    add play and pause features

---
+++ script.js
@@ -0,0 +1,28 @@
+const inputText = document.getElementById('text-input')
+const speedInput = document.getElementById('speed-input')
+const playButton = document.getElementById('play-button')
+const pauseButton = document.getElementById('pause-button')
+const resetButton = document.getElementById('reset-button')
+
+playButton.addEventListener('click', (event) => {
+  if (speechSynthesis.paused && speechSynthesis.speaking) {
+    speechSynthesis.resume()
+  } else {
+    const utterance = new SpeechSynthesisUtterance(inputText.value)
+    speechSynthesis.speak(utterance)
+
+    if (speechSynthesis.speaking) {
+      inputText.setAttribute('disabled', '')
+    }
+
+    utterance.addEventListener('end', (event) => {
+      inputText.removeAttribute('disabled')
+    })
+  }
+})
+
+pauseButton.addEventListener('click', (event) => {
+  if (speechSynthesis.speaking) {
+    speechSynthesis.pause()
+  }
+})
